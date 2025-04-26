617e1f3d42b9be4c36a6998437cfcce0ec6361d4
kader009 <kadermollarasel000@gmail.com>
2025-04-26 09:47:52 +0600

    single product data get

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,21 +1,28 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import {useParams} from 'react-router'
+import { useParams } from 'react-router';
 
 const ProductDetail = () => {
   const { id } = useParams();
-  const [SingleProduct, setSingleProduct] = useState(null);
+  const [SingleProduct, setSingleProduct] = useState([]);
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
         const response = await axios.get(
-          `https://admin.refabry.com/api/all/product/get/${id}`
+          `https://admin.refabry.com/api/all/product/get`
         );
-        console.log(response.data);
-        setSingleProduct(response.data);
+        
+        const allproduct = response.data.data.data;
+        const single = allproduct.find((p) => p.id === parseInt(id, 10));
+
+        if (!single) {
+          console.warn(`Product with id=${id} not found in list.`);
+        }
+
+        setSingleProduct(single);
       } catch (error) {
-        console.error('There was an error fetching the food data!', error); 
+        console.error('There was an error fetching the food data!', error);
       }
     };
 
@@ -23,9 +30,7 @@ const ProductDetail = () => {
   }, [id]);
 
   console.log(SingleProduct);
-  return (
-    <div>ProductDetail</div>
-  )
-}
+  return <div>ProductDetail</div>;
+};
 
-export default ProductDetail
+export default ProductDetail;
